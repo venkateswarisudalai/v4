3d19355a5896f0ca339a96712be36ebf4fa3664c
venkateswarisudalai <venki10march@gmail.com>
2020-08-01 19:26:41 +0530

    added few links

--- src/config.js
+++ src/config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  siteTitle: 'Brittany Chiang | Software Engineer',
+  siteTitle: 'venkateswari | Devops Engineer',
   siteDescription:
-    'Brittany Chiang is a software engineer based in Boston, MA who specializes in building (and occasionally designing) exceptional websites, applications, and everything in between.',
+    'venkateswari is a software engineer based in banglore, MA who specializes in building and managing infrastructure.',
   siteKeywords:
-    'Brittany Chiang, Brittany, Chiang, bchiang7, software engineer, front-end engineer, web developer, javascript, northeastern',
-  siteUrl: 'https://brittanychiang.com',
+    'Venkateswari sudalai, venkateswari, sudalai, Devops engineer, Site realiability engineer, SRE,',
+  siteUrl: 'https://Venkateswari.com',
   siteLanguage: 'en_US',
   googleAnalyticsID: 'UA-45666519-2',
   googleVerification: 'DCl7VAf9tcz6eD9gb67NfkNnJ1PKRNcg8qQiwpbx9Lk',
-  name: 'Brittany Chiang',
-  location: 'Boston, MA',
-  email: 'brittany.chiang@gmail.com',
-  github: 'https://github.com/bchiang7',
-  twitterHandle: '@bchiang7',
+  name: 'Venkateswari',
+  location: 'Banglore, KA',
+  email: 'venkateswari1095@gmail.com',
+  github: 'https://github.com/venkateswarisudalai',
+  twitterHandle: '@venkateswari_s',
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
+      url: 'https://github.com/venkateswarisudalai',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
+      url: 'https://www.linkedin.com/in/venkateswari-sudalai-371a77bb/',
     },
     {
       name: 'Codepen',
@@ -28,11 +28,11 @@ module.exports = {
     },
     {
       name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
+      url: 'https://www.instagram.com/venkateswari_s',
     },
     {
       name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
+      url: 'https://twitter.com/venkateswaris',
     },
   ],
 
